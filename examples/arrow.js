89d6580ee01f2eee0ce9455d85be5598f0cc6e62
Alissa Likavec <alissalikavec@gmail.com>
2016-12-19 12:28:41 -0800

    Added more arrow function basics

--- examples/arrow.js
+++ examples/arrow.js
@@ -1,3 +1,21 @@
+/*
+  Arrow functions are shorter ways of declaring a function compared to function
+  expressions. However they have a few key differences:
+  - Arrow functions are always anonymous
+  - parentheses are optional with only a single parameter
+  - cannot be used as constructors (e.g. function Person(){}), and will
+    throw an error if you try to use 'new'
+  - does not bind 'this', see https://mzl.la/2felxSE
+  - does not bind arguments object
+  - implicitly returns if no block body is given,
+    also known as a "concise body", e.g.
+    var foo = (x, y) => x + y; // returns x + y
+
+    otherwise:
+    var foo = (x, y) => { return x + y }
+*/
+
+
 var arrow = {
   findOdds: function( arr ){
     // go through arr using forEach and an arrow function
